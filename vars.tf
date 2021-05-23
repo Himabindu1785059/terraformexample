0e04c3a114ad01ab51407656dcc95e0b1d8597aa
Himabindu1785059 <84077619+Himabindu1785059@users.noreply.github.com>
2021-05-23 14:38:03 +0530

    terraform demo

---
+++ vars.tf
@@ -0,0 +1,15 @@
+
+variable "ami" {
+
+  default = "ami-02ec32cd830552285"
+}
+
+variable "instance_count" {
+  default = "0"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+//demo man
